d1531eb80f5e8c04d3fd8b70d50be56aa79a94a3
Matt Glover <matt@duchysoftware.com>
2014-03-18 18:40:42 +0000

    Initial Commit

---
+++ KiwiTicTacToe/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  KiwiTicTacToe
+//
+//  Created by Matt Glover on 18/03/2014.
+//  Copyright (c) 2014 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KiwiTicTacToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KiwiTicTacToe
+//
+//  Created by Matt Glover on 18/03/2014.
+//  Copyright (c) 2014 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTTAppDelegate class]));
+    }
+}
