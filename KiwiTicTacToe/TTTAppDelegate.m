8ec596a7ad5fdee40285e6c1bc0b1b11aa67caab
Matt Glover <matt@duchysoftware.com>
2015-09-29 14:19:53 +0200

    Changed a test to fail. Enabled Code Coverage

--- KiwiTicTacToe/TTTAppDelegate.m
+++ KiwiTicTacToe/TTTAppDelegate.m
@@ -15,6 +15,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    UIViewController *viewController = [[UIViewController alloc] init];
+    self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
